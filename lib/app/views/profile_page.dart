6ce50260aabdb59af85cbefab4270a6f33841ae7
Pratik K <undopatika@gmail.com>
2023-07-26 00:56:43 +0530

    refactor: simplify ProfilePage layout (#7)

--- lib/app/views/profile_page.dart
+++ lib/app/views/profile_page.dart
@@ -14,68 +14,19 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      child: Center(
-        child: Padding(
-          padding: const EdgeInsets.only(top: 18.0),
-          child: Container(
-            height: 560.h,
-            width: 420.w,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(5),
-              color: Color.fromARGB(255, 248, 247, 252),
-              boxShadow: const [
-                BoxShadow(
-                  color: Color(0x3F000000),
-                  blurRadius: 4,
-                  offset: Offset(0, 4),
-                  spreadRadius: 0,
-                )
-              ],
-            ),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 15.h,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 5, left: 24),
-                  child: Text(
-                    'Profile Settings',
-                    style: GoogleFonts.roboto(
-                        fontSize: 18.sp,
-                        fontWeight: FontWeight.w500,
-                        height: 1,
-                        color: const Color(0xFF323134).withAlpha(230),
-                        letterSpacing: 0.5),
-                  ),
-                ),
-                const Divider(
-                  color: Color(0xFFD9D9D9),
-                  thickness: 3,
-                ),
-                SizedBox(
-                  width: 500.w,
-                  height: 480.h,
-                  child: ListView(
-                    shrinkWrap: true,
-                    children: [
-                      listItems(Icons.email_outlined, 'Update Email'),
-                      listItems(Icons.phone_android, 'Update Mobile'),
-                      listItems(Icons.devices, 'Update Mac'),
-                      listItems(Icons.edit_location_outlined, 'Update Address'),
-                      listItems(Icons.lock_outline, 'Change Password'),
-                      listItems(Icons.location_on_outlined, 'My Geo Location'),
-                      listItems(Icons.notifications_active_outlined,
-                          'Push Notification'),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        ),
+    return Padding(
+      padding: const EdgeInsets.all(3.0),
+      child: ListView(
+        shrinkWrap: true,
+        children: [
+          listItems(Icons.email_outlined, 'Update Email'),
+          listItems(Icons.phone_android, 'Update Mobile'),
+          listItems(Icons.devices, 'Update Mac'),
+          listItems(Icons.edit_location_outlined, 'Update Address'),
+          listItems(Icons.lock_outline, 'Change Password'),
+          listItems(Icons.location_on_outlined, 'My Geo Location'),
+          listItems(Icons.notifications_active_outlined, 'Push Notification'),
+        ],
       ),
     );
   }
