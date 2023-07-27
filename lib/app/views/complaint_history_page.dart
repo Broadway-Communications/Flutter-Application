6b3bf6afbbd205ef34595cb1e437a9245108631d
Pratik K <undopatika@gmail.com>
2023-07-27 12:14:36 +0530

    feat: add correct layout to complaint history page (#18)

---
+++ lib/app/views/complaint_history_page.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:auto_route/annotations.dart';
+
+@RoutePage()
+class ComplaintHistoryPage extends StatelessWidget {
+  const ComplaintHistoryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent, 
+        statusBarIconBrightness: Brightness.light,
+      ),
+      child: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment(-1.0, 0.0),
+              end: Alignment(1.0, 0.0),
+              colors: [Color(0xff001422), Color(0xff3D579D)],
+              stops: [0, 1],
+            ),
+          ),
+          child: SafeArea(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 10.0),
+                  child: AppBar(
+                    backgroundColor: Colors.transparent,
+                    elevation: 0,
+                    leading: IconButton(
+                      icon: const Icon(Icons.arrow_back_ios_new),
+                      color: Colors.white,
+                      onPressed: () => Navigator.of(context).pop(),
+                    ),
+                    title: const Text(
+                      'Complaint History',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 21,
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    color: Colors.white, 
+                    child: const Center(
+                      child: Text('Your complaint history will appear here.'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
