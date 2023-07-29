9e2d526e35b0ee28339bc02dbdb9b71e9163632d
Pratik K <undopatika@gmail.com>
2023-07-29 14:25:45 +0530

    feat: add layout for invoice history page (#26)

---
+++ lib/app/views/invoice_history_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class InvoiceHistoryPage extends StatelessWidget {
+  const InvoiceHistoryPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+      value: const SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent, 
+        statusBarIconBrightness: Brightness.light,
+      ),
+      child: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment(-1.0, 0.0),
+              end: Alignment(1.0, 0.0),
+              colors: [Color(0xff001422), Color(0xff3D579D)],
+              stops: [0, 1],
+            ),
+          ),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(bottom: 10.0),
+                child: AppBar(
+                  backgroundColor: Colors.transparent,
+                  elevation: 0,
+                  leading: IconButton(
+                    icon: const Icon(Icons.arrow_back_ios_new),
+                    color: Colors.white,
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                  title: const Text(
+                    'Invoice History',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 21,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.white, 
+                  child: const Center(
+                    child: Text('Your invoice history will appear here.'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
